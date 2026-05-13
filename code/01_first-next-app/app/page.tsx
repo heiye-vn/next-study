446432443ae74c79e4f2c6d126cabd4089047aef
zsp_777 <1064239893@qq.com>
2026-05-13 23:07:17 +0800

    next中的路由导航

--- code/01_first-next-app/app/page.tsx
+++ code/01_first-next-app/app/page.tsx
@@ -1,5 +1,8 @@
-import Image from "next/image";
+import Link from "next/link"
 
 export default function Page() {
-  return <h1 className="text-4xl font-semibold text-red-500  px-2.5 py-5">Hello, Next.js!</h1>
+  return <>
+    <h1 className="text-4xl font-semibold text-red-500  px-2.5 py-5">Hello, Next.js!</h1>
+    <Link href="/dashboard">Dashboard</Link>
+  </>
 }
